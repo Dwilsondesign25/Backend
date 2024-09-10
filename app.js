ae6c66ce9cdc32f30bd99bf00846ad00e038eae4
Dwilsondesign25 <dwilsondesign25@gmail.com>
2024-09-10 13:11:37 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import fs from "fs";
+
+
+// const express = require("express");
+
+const app = express();
+
+app.get("/",(req, res) => {
+        res.send("Hello Angular Devs!");
+    })
+    .get("/users", getUsers)
+
+console.log("test")
+
+app.listen(3000,() => {
+    console.log("Server is running on http://localhost.3000");
+})
+
+function getUsers(){
+    fs.readFile("users.json", { encoding: "utf-8" }, (err, results) => {
+        console.log(results);
+    })
+}
